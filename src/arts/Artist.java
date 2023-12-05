987016e00899af24ba435ceb8dc067018ee63354
Jemjem7 <benzjeramtinga777@gmail.com>
2023-12-06 00:11:18 +0800

    added the last java/ artist.java

--- src/arts/Artist.java
+++ src/arts/Artist.java
@@ -2,7 +2,7 @@ package arts;
 
 public class Artist {
     public void createArt() {
-        System.out.println("Creating arts with my friend");
+        System.out.println("Creating arts with my friend arjie");
                 System.out.println();
 
     }
